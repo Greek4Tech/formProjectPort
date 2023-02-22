959e242f5da029082820400aa6fb58bc09ebf7db
Greek4Tech <97710680+Greek4Tech@users.noreply.github.com>
2023-02-22 12:00:00 -0600

    comments

--- client/src/App.js
+++ client/src/App.js
@@ -85,7 +85,7 @@ const App = () => {
       return accumulator + item.quantity * item.price;
     }, 0);
   };
-  
+
  // Define a function to remove a specific line item from the line
   const removeALine = (index) => {
     const copyOfLineList = [...lineOfItems];
@@ -121,6 +121,8 @@ const App = () => {
           className="mt-1 inline-flex justify-center w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:py-1.5 sm:text-sm sm:leading-6"
         ></textarea>
         <h2>List of Items</h2>
+        
+        {/* Mapping over array of items and generating a div with inputs for each */}
         {lineOfItems.map((item, index) => (
           <div className = "md:flex" key={index}>
             {/* Description */}
