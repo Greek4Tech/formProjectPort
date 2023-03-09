32fa41131245bf29f492a139f533b1e79b984f11
Greek4Tech <97710680+Greek4Tech@users.noreply.github.com>
2023-03-09 12:00:00 -0600

    comments

--- client/src/App.js
+++ client/src/App.js
@@ -100,7 +100,7 @@ const App = () => {
         className="inline-flex justify-center text-sm font-medium leading-6 text-gray-900"
         htmlFor="name">Name</label>
         <input 
-        // className - Input - Name
+        // Tailwind classNames
         className="inline-flex justify-center w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
           type="text" 
           id="name" 
