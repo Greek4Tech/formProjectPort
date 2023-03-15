77ae56c98232ee1c7d4b600793e256ae20bc31cd
Greek4Tech <97710680+Greek4Tech@users.noreply.github.com>
2023-03-15 18:37:33 -0500

    replit code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,119 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [name, setName] = useState("");
+  const [address, setAddress] = useState("");
+  const [lineOfItems, setLineOfItems] = useState([
+    { description: "", quantity: 1, price: 0 }
+  ]);
+  const [formValidated, setFormValidated] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.stopPropagation();
+    } else {
+      console.log(name, address, lineOfItems)
+    }
+    setFormValidated(true);
+  };
+
+  const changeName = (event) => {
+    setName(event.target.value);
+  };
+
+  const changeAddress = (event) => {
+    setAddress(event.target.value);
+  };
+
+  const handleChangeLine = (index, field, value) => {
+    const copyOflineOfItems = [...lineOfItems];
+    copyOflineOfItems[index][field] = value;
+    setLineOfItems(copyOflineOfItems);
+  };
+
+  const addALine = () => {
+    const copyOfLineList = [...lineOfItems];
+    setLineOfItems([
+      ...copyOfLineList,
+      {
+        description: "",
+        quantity: 1,
+        price: 0
+      }
+    ]);
+  };
+
+  const total = () => {
+    return lineOfItems.reduce((accumulator, item) => {
+      return accumulator + item.quantity * item.price;
+    }, 0);
+  };
+
+  const removeALine = (index) => {
+    const copyOfLineList = [...lineOfItems];
+    copyOfLineList.splice(index, 1);
+    setLineOfItems(copyOfLineList);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form onClick={handleSubmit} >
+        <label htmlFor="name">Name</label>
+        <input type="text" id="name" required value={name} onChange={changeName} />
+        <label htmlFor="address">Address</label>
+        <textarea
+          type="text"
+          id="address"
+          value={address}
+          onChange={changeAddress}
+          required
+        ></textarea>
+        <h2>List of Items</h2>
+        {lineOfItems.map((item, index) => (
+          <div key={index}>
+            {/* Description */}
+            <label htmlFor={`description${index}`}>Description</label>
+            <input
+              id={`description${index}`}
+              value={item.description}
+              type="text"
+              onChange={(event) => {
+                handleChangeLine(index, "description", event.target.value);
+              }}
+            />
+            {/* quantity */}
+            <label htmlFor={`quantity${index}`}>Quantity</label>
+            <input
+              type="number"
+              id={`quantity${index}`}
+              value={item.quantity}
+              onChange={(event) =>
+                handleChangeLine(index, "quantity", event.target.value)
+              }
+            />
+            {/* price */}
+            <label htmlFor={`price${index}`}>Price</label>
+            <input
+              type="number"
+              id={`price${index}`}
+              value={item.price}
+              onChange={(event) =>
+                handleChangeLine(index, "price", event.target.value)
+              }
+            />
+            <button onClick={() => removeALine(index)}>Remove a Line</button>
+          </div>
+        ))}
+        <div>
+          <button onClick={addALine}>Add A line</button>
+          <button type="submit">Submit</button>
+        </div>
+        <p>The total is: {total()}</p>
+      </form>
+    </>
   );
-}
+};
 
-export default App;
+export default App; 
