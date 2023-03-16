61123b51a4599e1be5dfdd61dbdc4f57c7b66823
Greek4Tech <97710680+Greek4Tech@users.noreply.github.com>
2023-03-16 12:35:37 -0500

    tailwind label/input for Name

--- src/App.js
+++ src/App.js
@@ -54,8 +54,10 @@ const App = () => {
   return (
     <>
       <form onSubmit={handleSubmit} >
-        <label htmlFor="name">Name</label>
+        <label className="block text-sm font-medium leading-6 text-gray-900"
+        htmlFor="name">Name</label>
         <input 
+        className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
           type="text" 
           id="name" 
           minlength="1" 
