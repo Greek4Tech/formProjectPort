10b75bb66e91ab29b7ba7c8d62b20b7748163c4d
Greek4Tech <97710680+Greek4Tech@users.noreply.github.com>
2023-03-17 08:18:43 -0500

    added type=button

--- src/App.js
+++ src/App.js
@@ -129,7 +129,7 @@ const App = () => {
           </div>
         ))}
         <div>
-          <button  className="inline-flex justify-center rounded-md bg-indigo-600 py-2 px-3 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+          <button type="button" className="inline-flex justify-center rounded-md bg-indigo-600 py-2 px-3 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
           onClick={addALine}>Add A line</button>
           {/* <button type="button">Submit</button> */}
         </div>
