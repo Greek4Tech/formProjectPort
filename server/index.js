708574d3b8117f00529dbe0de774b745fbbbebb4
Greek4Tech <97710680+Greek4Tech@users.noreply.github.com>
2023-03-29 09:40:16 -0500

    data

--- server/index.js
+++ server/index.js
@@ -5,6 +5,12 @@ const port = 4000;
 
 app.use(cors())
 
+const data = [
+  { description: "apples", quantity: 1, price: 100 },
+  { description: "mayo", quantity: 1, price: 250 },
+  { description: "oranges", quantity: 1, price: 400 },
+]
+
 app.get('/', (req, res) => {
   res.json('Hello World!');
 });
