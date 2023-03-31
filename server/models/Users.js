f2990ba71b9d435839556815f5a1a61287380e53
Greek4Tech <97710680+Greek4Tech@users.noreply.github.com>
2023-03-31 08:21:57 -0500

    pass local and bc

---
+++ server/models/Users.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+// Create Schema
+const UserSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        email_is_verified: {
+            type: Boolean,
+            default: false
+        },
+        password: {
+            type: String
+        }
+    }
+)
