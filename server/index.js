a245c0b97ac48113a24a89d8e1dabb42fee673aa
Greek4Tech <97710680+Greek4Tech@users.noreply.github.com>
2023-04-04 13:28:23 -0500

    sadf~
    cd ..

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const app = express();
 const axios = require('axios')
 const cors = require('cors')
 const port = 4000;
+const bcrypt = require("bcrypt");
+
 
 // loads environment variables from a .env
 require('dotenv').config({ path: './config/.env' });
@@ -16,6 +18,9 @@ const dbConnect = require("./config/db/dbConnect");
 
 // execute database connection 
 dbConnect();
+
+// User Schema
+const User = require("./config/db/userModel");
 
 app.use(cors())
 app.use(express.json());
@@ -34,6 +39,45 @@ app.use(session({
 
 app.get('/', (req, res) => {
   res.json('Hello World!');
+});
+
+// register endpoint
+app.post("/register", (request, response) => {
+  // hash the password
+  bcrypt
+    .hash(request.body.password, 10)
+    .then((hashedPassword) => {
+      // create a new user instance and collect the data
+      const user = new User({
+        email: request.body.email,
+        password: hashedPassword,
+      });
+
+      // save the new user
+      user
+        .save()
+        // return success if the new user is added to the database successfully
+        .then((result) => {
+          response.status(201).send({
+            message: "User Created Successfully",
+            result,
+          });
+        })
+        // catch error if the new user wasn't added successfully to the database
+        .catch((error) => {
+          response.status(500).send({
+            message: "Error creating user",
+            error,
+          });
+        });
+    })
+    // catch error if the password hash isn't successful
+    .catch((e) => {
+      response.status(500).send({
+        message: "Password was not hashed successfully",
+        e,
+      });
+    });
 });
 
 app.post('/charge', (req, res) => {
