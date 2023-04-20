29480cd41a3fea2b1a6979553ba2ae9435666e7c
Greek4Tech <97710680+Greek4Tech@users.noreply.github.com>
2023-04-20 17:22:01 -0500

    sign up

--- client/src/Signup.js
+++ client/src/Signup.js
@@ -136,7 +136,7 @@ export default function Signup() {
                 <span className="absolute inset-y-0 left-0 flex items-center pl-3">
                   <LockClosedIcon className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true" />
                 </span>
-                Sign in
+                Sign Up
               </button>
             </div>
           </form>
